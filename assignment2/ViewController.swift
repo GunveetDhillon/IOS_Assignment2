09f212620f1bb797714eeee5fe831b2a9e4a3d3f
GunveetDhillon <dhillon.gunu@gmail.com>
2023-07-09 00:38:51 -0400

    Initial Commit

---
+++ assignment2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  assignment2
+//
+//  Created by Gunveet Singh Dhillon on 2023-07-09.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
